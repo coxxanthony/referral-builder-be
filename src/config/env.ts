6e57ea8d54f830d26d8d43b714494ec8d4505845
cox <coxleehubilla@gmail.com>
2026-02-06 19:09:03 +0800

    Remove API_URL from config

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,4 @@ export const config = {
   HOST: process.env.HOST || "localhost",
   PORT: process.env.PORT || 5000,
   DATABASE_URL: process.env.DATABASE_URL || "",
-  API_URL: process.env.API_URL || "http://localhost:3000",
 };
